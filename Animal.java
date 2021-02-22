b057f12cfdf546333250523f968118fb92549204
aceito <glo-mac@aadams-mbp.lan>
2021-02-22 17:51:50 +0000

    test comment

--- Animal.java
+++ Animal.java
@@ -6,6 +6,7 @@ import java.util.List;
  * @author David J. Barnes and Michael Kölling
  * @version 2016.02.29 (2)
  */
+//test comment
 public abstract class Animal
 {
     // Whether the animal is alive or not.
